fa68eb7f8c71c171469b6264de54f28ac9e5063e
Guerric Cochelin <cochelin.guerric@hotmail.com>
2025-01-12 23:09:49 +0100

    test

--- js/data_projets.js
+++ js/data_projets.js
@@ -6,29 +6,23 @@ export const projets_pages = [
         "tag": "FULL HTML",
         "content": 
         "<h2>Le Site XYZ : Une Révolution du Web</h2>\n" +
-        "<p>Le site XYZ a été conçu pour offrir une expérience utilisateur unique. Il se distingue par son design moderne et sa facilité d'utilisation, tout en offrant des fonctionnalités de pointe adaptées aux besoins des utilisateurs dans le secteur de la technologie.</p>\n" +
         "<div class='section-line'></div>\n" +
         "<section>\n" +
         "<h3>Contexte</h3>\n" +
-        "<p>Le site XYZ a été créé dans le cadre d'un projet visant à résoudre les problèmes de navigation et de fonctionnalité des sites web existants dans le domaine des technologies. Il a été conçu pour être accessible sur tous les appareils et optimiser la vitesse de navigation.</p>\n" +
+        "<p>Le site XYZ a été créé pour résoudre les problèmes de navigation et de fonctionnalité des sites web existants dans le domaine des technologies.</p>\n" +
         "</section>\n" +
         "<div class='section-line'></div>\n" +
         "<section>\n" +
         "<h3>Mission</h3>\n" +
-        "<p>La mission de XYZ est de fournir aux utilisateurs une plateforme intuitive qui leur permette de trouver rapidement les informations qu'ils recherchent, tout en améliorant la performance globale du site grâce à des optimisations avancées de son infrastructure.</p>\n" +
+        "<p>La mission de XYZ est de fournir une plateforme intuitive qui améliore la performance globale grâce à des optimisations avancées.</p>\n" +
         "</section>\n" +
         "<div class='section-line'></div>\n" +
-        "<section>\n" +
-        "<h3>Résultat</h3>\n" +
-        "<p>Depuis son lancement, XYZ a vu une augmentation significative du nombre d'utilisateurs actifs et a reçu des retours très positifs concernant sa facilité d'utilisation et sa rapidité. Le site est devenu une référence dans son domaine.</p>\n" +
         "<div class='iframe-container'>\n" +
         "    <iframe id='result-iframe' src='../la-martiniere-monplaisir/page1.html' title='Site XYZ'></iframe>\n" +
         "    <button id='activate-iframe' class='iframe-button'>Activer le contenu</button>\n" +
-        "</div>\n" +
-        "</section>\n" +
-        "<div class='section-line'></div>\n",
+        "</div>\n",
         "slug": "la-martiniere-monplaisir",
-        "metaDescription": "Découvrez le voyage interplanétaire vers la planète b2seomds. Explorez les défis, les technologies nécessaires et les risques d'une mission spatiale fascinante."
+        "metaDescription": "Découvrez le voyage interplanétaire vers la planète b2seomds."
     }
 ];
 
