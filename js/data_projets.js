e72ee2ee3ea9ce7c3b19e7d3b5e1b8b7b95a437c
Guerric Cochelin <cochelin.guerric@hotmail.com>
2025-01-17 04:32:35 +0100

    end la marti

--- js/data_projets.js
+++ js/data_projets.js
@@ -8,14 +8,14 @@ export const projets_pages = [
 "<div class='section-line'></div>\n" +
 "<section>\n" +
 "<h3>Contexte</h3>\n" +
-"<p>Lors de ma première année à My-Digital-School, dans le cadre du premier partiel, nous avons reçu pour mission de choisir un lycée et de travailler sur plusieurs aspects de sa communication. Cela incluait la refonte complète de sa charte graphique, l’élaboration d’une nouvelle stratégie de communication, ainsi que la réalisation d’un site web moderne. Nous avions la liberté de choisir entre une implémentation via WordPress ou un développement entièrement codé à la main.</p>\n" +
+"<p>Lors de ma première année à My-Digital-School, dans le cadre du premier partiel sur une semaine, nous avons reçu pour mission de choisir un lycée et de travailler sur plusieurs aspects de sa communication. Cela incluait la refonte complète de sa charte graphique, l’élaboration d’une nouvelle stratégie de communication, ainsi que la réalisation d’un site web moderne. Nous avions la liberté de choisir entre une implémentation via WordPress ou un développement entièrement codé à la main.</p>\n" +
 "<p>Dans notre groupe, j’occupais le rôle de développeur. À partir d’une maquette conçue par un collègue, j’ai pris en charge la réalisation technique du site web en HTML et CSS.</p>\n" +
 "</section>\n" +
 "<div class='section-line'></div>\n" +
 "<section>\n" +
 "<h3>Ce que j'ai appris</h3>\n" +
 "<div class='text-and-code'>\n" +
-"<p>Depuis son lancement, XYZ a vu une augmentation significative du nombre d'utilisateurs actifs et a reçu des retours très positifs concernant sa facilité d'utilisation et sa rapidité. Le site est devenu une référence dans son domaine.</p>\n" +
+"<p>Dans ce projet, j'ai appris à utiliser la propriété CSS <strong>z-index</strong> en combinaison avec <strong>position: absolute;</strong> pour gérer la superposition d'éléments. Par exemple, dans le header du site, un logo devait être positionné en plein milieu tout en restant au-dessus d'autres éléments comme des bandes de couleur ou des ronds décoratifs. Grâce à <code>position: absolute;</code>, j'ai pu placer le logo exactement où je voulais sur la page, tandis que <code>z-index</code> m'a permis de contrôler l'ordre d'affichage des éléments, en veillant à ce que le logo soit toujours visible, même si d'autres éléments se superposaient sous lui.</p>\n" +
 "<pre><code class='language-css'>\n" + 
 "#menu-image img {\n" +
 "    width: 230px;\n" +
@@ -33,7 +33,7 @@ export const projets_pages = [
 "<div class='section-line'></div>\n" +
 "<section>\n" +
 "<h3>Résultat</h3>\n" +
-"<p>Depuis son lancement, XYZ a vu une augmentation significative du nombre d'utilisateurs actifs et a reçu des retours très positifs concernant sa facilité d'utilisation et sa rapidité. Le site est devenu une référence dans son domaine.</p>\n" +
+"<p>En activant le contenu, vous pouvez voir le rendu final. Il y a trois pages accessibles : la page Lycée, Formation et Vie scolaire.</p>\n" +
 "<div class='iframe-container'>\n" +
 "    <iframe id='result-iframe' src='../la-martiniere-monplaisir/page1.html' title='Site XYZ'></iframe>\n" +
 "    <button id='activate-iframe' class='iframe-button'>Activer le contenu</button>\n" +
