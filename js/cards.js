02edf3b67c6c3c1f74299fe2e90989f05e99ea92
Guerric Cochelin <cochelin.guerric@hotmail.com>
2025-03-05 23:32:14 +0100

    o

--- js/cards.js
+++ js/cards.js
@@ -28,7 +28,7 @@ const projets = [
     },
     {
         "description": "Utilisation d'une API",
-        "url": "projets/pokedex",
+        "url": "projets/summonersdle",
         "src": "img/pokedex.png",
         "alt": "Pokedex",
         "title": "Pokedex",
